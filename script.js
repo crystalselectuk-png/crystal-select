af98efe3b01f1f62e493167e25e7ff8793c86cb2
crystalselectuk-png <crystalselect.uk@gmail.com>
2026-07-20 00:51:57 +0100

    Update script.js 20-07-2026_1

--- script.js
+++ script.js
@@ -57,3 +57,25 @@ window.addEventListener('scroll', () => {
 });
 
 }); 
+
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('open');
+
+  const isOpen = navLinks.classList.contains('open');
+  menuToggle.setAttribute('aria-expanded', isOpen);
+
+  menuToggle.innerHTML = isOpen
+    ? '<i class="fa-solid fa-xmark"></i>'
+    : '<i class="fa-solid fa-bars"></i>';
+});
+
+navLinks.querySelectorAll('a').forEach(link => {
+  link.addEventListener('click', () => {
+    navLinks.classList.remove('open');
+    menuToggle.setAttribute('aria-expanded', 'false');
+    menuToggle.innerHTML = '<i class="fa-solid fa-bars"></i>';
+  });
+});
